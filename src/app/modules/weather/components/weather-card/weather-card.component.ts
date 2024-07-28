a9894df099319056979ac92133960e80ff2e9f8b
Nelson <punkpunkada@gmail.com>
2024-07-28 14:45:38 -0300

    editing weather card

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherData } from 'src/app/models/interfaces/weather-data';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent{
+  //Dados da previsão do tempo recebidos do componente pai
+  @Input() weatherInput!: WeatherData;
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+
+}
